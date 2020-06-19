1080671941297a0abe6ae7460e8eb3f424830baa
HoodedPitohui <58968966+HoodedPitohui@users.noreply.github.com>
2020-06-18 20:40:42 -0700

    Size Editing

---
+++ javascript/function.js
@@ -0,0 +1,100 @@
+function.jsJavaScript
+// Code By Webdevtrick ( https://webdevtrick.com )
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+$('#fullpage').fullpage({
+  sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  menu: '#menu',
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+
+});
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+// Code By Webdevtrick ( https://webdevtrick.com )
+var $header_top = $('.header-top');
+var $nav = $('nav');
+ 
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+ 
+$('#fullpage').fullpage({
+  sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  menu: '#menu',
+ 
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+ 
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+ 
+ 
+});
